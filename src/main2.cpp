e78337dd89b3ef3143965fe037432b5a5300eee9
tujin <tujin@mail.ru>
2019-03-20 01:15:02 +0300

    12345

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,8 +1,13 @@
 #include "task2.h"
 #include <iostream>
-
+template<class T>
+T gen()
+{
+    static int t = 48;
+    return t++;
+}
 int main() {
-	auto * a = createArr<int, 10>(gen);
+	auto * a = createArr<int, 10>(*gen);
 
 	for (int i = 0; i < 10; i++)
 		std::cout << a[i] << " ";
